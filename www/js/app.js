9eb48e725b07431774fbca80077a228b0c1885ac
Ganesh Prasad <g.c.prasad2gmail.com>
2015-08-28 15:14:51 +1000

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,10 @@
+angular.module('App1', ['ionic'])
+
+.controller('App1Control', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+});
